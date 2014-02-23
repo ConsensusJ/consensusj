acbea7b457b2a86694d588e79f3d7c4688e1915e
Sean Gilligan <sean@msgilligan.com>
2014-02-22 23:09:26 -0800

    Use SeedPeers for PeerDiscovery.

--- src/main/java/com/msgilligan/peerlist/config/BitcoinConfig.java
+++ src/main/java/com/msgilligan/peerlist/config/BitcoinConfig.java
@@ -3,9 +3,12 @@ package com.msgilligan.peerlist.config;
 import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.net.discovery.DnsDiscovery;
 import com.google.bitcoin.net.discovery.PeerDiscovery;
+import com.google.bitcoin.net.discovery.SeedPeers;
 import com.google.bitcoin.params.MainNetParams;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.FileNotFoundException;
 
 /**
  * User: sean
@@ -20,7 +23,10 @@ public class BitcoinConfig {
     }
 
     @Bean
-    public PeerDiscovery peerDiscovery(NetworkParameters params) {
-        return new DnsDiscovery(params);
+    public PeerDiscovery peerDiscovery(NetworkParameters params) throws FileNotFoundException {
+        PeerDiscovery pd;
+//        pd = new DnsDiscovery(params);
+        pd = new SeedPeers(params);
+        return pd;
     }
 }
