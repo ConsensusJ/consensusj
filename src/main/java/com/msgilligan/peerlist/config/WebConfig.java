1f875ef4a19ce1b68f11ea5d49b0c6d84d55ec23
Sean Gilligan <sean@msgilligan.com>
2014-03-12 14:14:53 -0700

    Add viewController for index.html in WebConfig
    (and remove RootController which implemented the same thing)

--- src/main/java/com/msgilligan/peerlist/config/WebConfig.java
+++ src/main/java/com/msgilligan/peerlist/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.msgilligan.peerlist.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -17,6 +18,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
     }
 
 }
