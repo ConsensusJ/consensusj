9dae13e45d4d591e3f68d4347d3c2595bc315827
Sean Gilligan <sean@msgilligan.com>
2014-11-23 17:16:08 -0800

    Update to newer Spring, jcenter(), etc

--- src/main/java/com/msgilligan/peerlist/config/WebSocketConfig.java
+++ src/main/java/com/msgilligan/peerlist/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
@@ -24,6 +25,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp").withSockJS();
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000).setSendBufferSizeLimit(512 * 1024);
     }
 
     @Override
