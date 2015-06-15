f79c9b9a15b45ede59812e3b6ee4d8a3a3ac33c3
Sean Gilligan <sean@msgilligan.com>
2015-06-15 12:23:44 -0700

    Upgrade to Spring Boot 1.2.4
    * Simplify Gradle, rely on starters, version free
    
    Tested with `bootRun`.

--- src/main/java/com/msgilligan/peerlist/config/WebSocketConfig.java
+++ src/main/java/com/msgilligan/peerlist/config/WebSocketConfig.java
@@ -3,6 +3,8 @@ package com.msgilligan.peerlist.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -39,6 +41,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientOutboundChannel(ChannelRegistration registration) {
         registration.taskExecutor().corePoolSize(4).maxPoolSize(10);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        // TODO: ??
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+        // TODO: ??
     }
 
     @Override
