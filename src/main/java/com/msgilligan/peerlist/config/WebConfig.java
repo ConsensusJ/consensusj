083237b228a373ba89990a66f0f95e3c31524b80
Sean Gilligan <sean@msgilligan.com>
2015-06-15 12:42:29 -0700

    Package/serve static resources - JAR format

--- src/main/java/com/msgilligan/peerlist/config/WebConfig.java
+++ src/main/java/com/msgilligan/peerlist/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.msgilligan.peerlist.config;
 
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,16 +14,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index.html");
-    }
-
+public class WebConfig extends WebMvcAutoConfiguration {
 }
