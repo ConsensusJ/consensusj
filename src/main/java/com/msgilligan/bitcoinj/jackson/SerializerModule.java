a7c1515ac3a957bcac658a804ca382b4088f812c
Sean Gilligan <sean@msgilligan.com>
2015-06-16 09:28:28 -0700

    Changes accidentally left out of previous commit

--- src/main/java/com/msgilligan/bitcoinj/jackson/SerializerModule.java
+++ src/main/java/com/msgilligan/bitcoinj/jackson/SerializerModule.java
@@ -1,7 +1,25 @@
 package com.msgilligan.bitcoinj.jackson;
 
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ser.Serializers;
+
 /**
  *
  */
-public class SerializerModule {
+public class SerializerModule extends Module {
+    @Override
+    public String getModuleName() {
+        return "BitcoinJModule";
+    }
+
+    @Override
+    public Version version() {
+        return new Version(1, 0, 0, null, "com.msgilligan.bitcoinj", "jackson");
+    }
+
+    @Override
+    public void setupModule(SetupContext context) {
+        context.addSerializers(new BitcoinJSerializers());
+    }
 }
--- src/main/java/com/msgilligan/bitcoinj/jackson/TransactionSerializer.java
+++ src/main/java/com/msgilligan/bitcoinj/jackson/TransactionSerializer.java
@@ -1,7 +1,21 @@
 package com.msgilligan.bitcoinj.jackson;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.bitcoinj.core.Transaction;
+
+import java.io.IOException;
+
 /**
- *
+ * Custom Serialization of Transaction Peer class
  */
-public class TransactionSerializer {
+public class TransactionSerializer extends JsonSerializer<Transaction> {
+    @Override
+    public void serialize(Transaction value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+        jgen.writeStartObject();
+        jgen.writeStringField("hash", value.getHashAsString());
+        jgen.writeEndObject();
+    }
 }
