b3da1a4d5c90d9614cf7d72046c4ab94eb35183b
Sean Gilligan <sean@msgilligan.com>
2015-09-23 19:25:36 -0700

    Use bitcoinj constant for stdTxFee

--- bitcoinj-rpcclient/src/main/groovy/com/msgilligan/bitcoinj/test/BTCTestSupport.groovy
+++ bitcoinj-rpcclient/src/main/groovy/com/msgilligan/bitcoinj/test/BTCTestSupport.groovy
@@ -4,13 +4,14 @@ import com.msgilligan.bitcoinj.rpc.BitcoinClientDelegate
 import com.msgilligan.bitcoinj.rpc.Loggable
 import com.msgilligan.bitcoinj.rpc.conversion.BitcoinMath
 import org.bitcoinj.core.Coin
+import org.bitcoinj.core.Transaction
 
 /**
  * Test support functions intended to be mixed-in to Spock test specs
  */
 trait BTCTestSupport implements BitcoinClientDelegate, FundingSourceDelegate, Loggable {
     // TODO: set, or get and verify default values of the client
-    final Coin stdTxFee = 0.00010000.btc
+    final Coin stdTxFee = Transaction.REFERENCE_DEFAULT_MIN_TX_FEE
     final Coin stdRelayTxFee = 0.00001000.btc
     final Integer defaultMaxConf = 9999999
 
