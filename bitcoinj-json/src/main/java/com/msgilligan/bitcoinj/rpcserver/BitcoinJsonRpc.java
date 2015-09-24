2ad1d84063f965fafee42651b1917e51b17f2b82
Sean Gilligan <sean@msgilligan.com>
2015-09-24 00:20:49 -0700

    Fix comment typo, remove commented-out annotation.

--- bitcoinj-json/src/main/java/com/msgilligan/bitcoinj/rpcserver/BitcoinJsonRpc.java
+++ bitcoinj-json/src/main/java/com/msgilligan/bitcoinj/rpcserver/BitcoinJsonRpc.java
@@ -5,10 +5,9 @@ import com.msgilligan.bitcoinj.json.pojo.ServerInfo;
 /**
  * Standard Bitcoin JSON-RPC service
  */
-//@JsonRpcService("BitcoinRPC")
 public interface BitcoinJsonRpc {
     public Integer getblockcount();
     public Integer getconnectioncount();
-    // TODO: For some reason getinfo doesn't work if we say it retuns ServerInfo
+    // TODO: For some reason getinfo doesn't work if we say it returns ServerInfo
     public Object getinfo();
 }
