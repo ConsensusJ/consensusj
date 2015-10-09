52cfac134afe9bd6472aeed07783fd10b4ee0b31
Sean Gilligan <sean@msgilligan.com>
2015-10-08 19:00:45 -0700

    BlockchainSyncing impl that uses Blockcypher API

---
+++ bitcoinj-rpcclient/src/main/groovy/com/msgilligan/bitcoinj/rpc/BlockCypherSyncing.groovy
@@ -0,0 +1,14 @@
+package com.msgilligan.bitcoinj.rpc
+
+import groovy.json.JsonSlurper
+
+/**
+ * Implementation of BlockchainSyncing that uses BlockCypher.com API
+ */
+trait BlockCypherSyncing extends BlockchainSyncing {
+
+    Integer getReferenceBlockHeight() {
+        Integer height = new JsonSlurper().parse(new URL("https://api.blockcypher.com/v1/btc/main")).height
+        return height
+    }
+}
