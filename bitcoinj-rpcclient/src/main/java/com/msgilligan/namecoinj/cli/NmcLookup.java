e0327591876c95ca129da3614b2af50ba0c9a5ec
Sean Gilligan <sean@msgilligan.com>
2016-06-01 14:02:19 -0700

    Use namecoin.conf for RPC connection info

--- bitcoinj-rpcclient/src/main/java/com/msgilligan/namecoinj/cli/NmcLookup.java
+++ bitcoinj-rpcclient/src/main/java/com/msgilligan/namecoinj/cli/NmcLookup.java
@@ -1,6 +1,9 @@
 package com.msgilligan.namecoinj.cli;
 
 import com.msgilligan.bitcoinj.rpc.JsonRPCStatusException;
+import com.msgilligan.bitcoinj.rpc.RPCConfig;
+import com.msgilligan.bitcoinj.rpc.bitcoind.AppDataDirectory;
+import com.msgilligan.bitcoinj.rpc.bitcoind.BitcoinConfFile;
 import com.msgilligan.namecoinj.core.NMCMainNetParams;
 import com.msgilligan.namecoinj.json.pojo.NameData;
 import com.msgilligan.namecoinj.rpc.NamecoinClient;
@@ -9,6 +12,7 @@ import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.params.MainNetParams;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -17,16 +21,25 @@ import java.net.URISyntaxException;
  * Simple command-line tool to do Namecoin name lookup
  */
 public class NmcLookup {
-    public static String rpcName = "xxx";
-    public static String rpcPassword = "yyy";
 
     public static void main(String[] args) throws URISyntaxException, IOException, JsonRPCStatusException, AddressFormatException {
-        URI server = new URI("http://localhost:8336");
+        RPCConfig config = readConfig();
         NetworkParameters netParams = NMCMainNetParams.get();
-        NamecoinClient client = new NamecoinClient(netParams, server, rpcName, rpcPassword);
+        NamecoinClient client = new NamecoinClient(netParams,
+                config.getURI(), config.getUsername(), config.getPassword());
         NameData result = client.nameShow("d/beelin");
         System.out.println(result.getValue());
         Address owningAddress = result.getAddress();
         System.out.println("Owning Address: " + owningAddress);
+    }
+
+    /**
+     * Namecoin conf file uses same format as bitcoin.conf
+     * @return object containing RPC endpoint information
+     */
+    static RPCConfig readConfig() {
+        File file = new File(AppDataDirectory.forAppName("Namecoin"), "namecoin.conf");
+        BitcoinConfFile conf = new BitcoinConfFile(file);
+        return conf.read().getRPCConfig();
     }
 }
