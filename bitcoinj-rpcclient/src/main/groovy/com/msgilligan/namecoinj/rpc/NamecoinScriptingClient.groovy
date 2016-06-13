d6e1938ffc04197b994e76692f1392619c7a46d1
Sean Gilligan <sean@msgilligan.com>
2016-06-13 13:06:41 -0700

    Add NamecoinScriptingClient

---
+++ bitcoinj-rpcclient/src/main/groovy/com/msgilligan/namecoinj/rpc/NamecoinScriptingClient.groovy
@@ -0,0 +1,15 @@
+package com.msgilligan.namecoinj.rpc
+
+import com.msgilligan.bitcoinj.rpc.DynamicRPCFallback
+import com.msgilligan.namecoinj.rpc.NamecoinClient
+
+/**
+ * Namecoin RPC client for scripting
+ * No args constructor reads namecoin.conf
+ * Allows dynamic methods to access new RPCs or RPCs not implemented in Java client
+ */
+class NamecoinScriptingClient extends NamecoinClient implements DynamicRPCFallback {
+    public NamecoinScriptingClient() {
+        super( NamecoinClient.readConfig());
+    }
+}
