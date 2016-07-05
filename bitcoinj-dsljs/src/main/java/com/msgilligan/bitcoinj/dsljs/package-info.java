346bc1a466903125a70902c715155ccd3a81e06c
Sean Gilligan <sean@msgilligan.com>
2016-07-05 13:05:22 -0700

    Add package-info.java files for recently added packages

---
+++ bitcoinj-dsljs/src/main/java/com/msgilligan/bitcoinj/dsljs/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Prototype JavaScript scripting support via Nashorn and ScriptEngineFactory (JSR 223)
+ */
+package com.msgilligan.bitcoinj.dsljs;
---
+++ bitcoinj-rpcclient/src/main/java/com/msgilligan/bitcoinj/rpc/bitcoind/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Support for reading RPC client connection info from `bitcoin.conf` file
+ */
+package com.msgilligan.bitcoinj.rpc.bitcoind;
