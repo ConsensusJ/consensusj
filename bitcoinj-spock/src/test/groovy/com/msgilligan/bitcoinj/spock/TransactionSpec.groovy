df0318924727efe375d8345dea5587c6064ad5d4
Sean Gilligan <sean@msgilligan.com>
2016-07-14 20:33:34 -0700

    Fix and enable “Can create and serialize a transaction”
    TODO: Verify the Transactions OPCODES are correct

--- bitcoinj-spock/src/test/groovy/com/msgilligan/bitcoinj/spock/TransactionSpec.groovy
+++ bitcoinj-spock/src/test/groovy/com/msgilligan/bitcoinj/spock/TransactionSpec.groovy
@@ -1,6 +1,5 @@
 package com.msgilligan.bitcoinj.spock
 
-import groovy.util.logging.Slf4j
 import org.bitcoinj.core.Address
 import org.bitcoinj.core.Base58
 import org.bitcoinj.core.Coin
@@ -8,36 +7,30 @@ import org.bitcoinj.core.Context
 import org.bitcoinj.core.ECKey
 import org.bitcoinj.core.Sha256Hash
 import org.bitcoinj.core.Transaction
-import org.bitcoinj.core.TransactionInput
 import org.bitcoinj.core.TransactionOutPoint
 import org.bitcoinj.params.MainNetParams
 import org.bitcoinj.script.ScriptBuilder
-import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
 
-
 /**
- *
- * See Bitcoins the Hard Way
+ * Create and verify the transaction from Ken Shirriff's blog article
+ * See "Bitcoins the Hard Way"
  * http://www.righto.com/2014/02/bitcoins-hard-way-using-raw-bitcoin.html
  */
 @Stepwise
-@Slf4j
 class TransactionSpec extends Specification {
     static final mainNetParams = MainNetParams.get()
 
     // Input Values
     static final fromKeyWIF = "5HusYj2b2x4nroApgfvaSfKYZhRbKFH41bVyPooymbC6KfgSXdD"
     static final Address toAddr = new Address(mainNetParams, "1KKKK6N21XKo48zWKuQKXdvSsCf95ibHFa")
-    static final Sha256Hash utxo_id = new Sha256Hash("81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48")
+    static final Sha256Hash utxo_id = Sha256Hash.wrap("81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48")
     static final Coin txAmount = 0.00091234.btc
-    static final Coin txFee = 0.0001.btc
 
     // Values used for Verification
     static final fromAddrVerify = new Address(mainNetParams, "1MMMMSUb1piy2ufrSguNUdFmAcvqrQF8M5")
-    static final txVerify = "0100000001484d40d45b9ea0d652fca8258ab7caa42541eb52975857f96fb50cd732c8b481000000008a47304402202cb265bf10707bf49346c3515dd3d16fc454618c58ec0a0ff448a676c54ff71302206c6624d762a1fcef4618284ead8f08678ac05b13c84235f1654e6ad168233e8201410414e301b2328f17442c0b8310d787bf3d8a404cfbd0704f135b6ad4b2d3ee751310f981926e53a6e8c39bd7d3fefd576c543cce493cbac06388f2651d1aacbfcdffffffff0162640100000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac00000000"
 
     @Shared
     ECKey fromKey
@@ -50,34 +43,54 @@ class TransactionSpec extends Specification {
     }
 
     def "Can create an address from private key WIF"() {
-        given:
+        when: "we create a private key from WIF format string in the article"
         byte[] privKey = Arrays.copyOfRange(Base58.decodeChecked(fromKeyWIF), 1, 33);
         fromKey = new ECKey().fromPrivate(privKey, false)
-        log.info("fromKey ${fromKey}")
+
+        and: "we convert it to an address"
         fromAddress = fromKey.toAddress(mainNetParams)
 
-        expect:
+        then: "it is the address from the article"
         fromAddress == fromAddrVerify
     }
 
-    @Ignore("Doesn't verify, not yet sure why")
     def "Can create and serialize a transaction"() {
         when:
         Transaction tx = new Transaction(mainNetParams)
         TransactionOutPoint outPoint = new TransactionOutPoint(mainNetParams, 0, utxo_id)
         tx.addOutput(txAmount, toAddr)
-        // Assume standard transaction
         tx.addSignedInput(outPoint, ScriptBuilder.createOutputScript(fromAddress), fromKey);
-        log.info("tx = ${tx}")
 
 
         and: "We serialize the transaction"
         byte[] rawTx = tx.bitcoinSerialize()
 
-        then: "Output is as expected"
-        rawTx.encodeHex().toString() == txVerify
+        and: "We parse it into a new Transaction object"
+        Transaction parsedTx = new Transaction(mainNetParams, rawTx)
 
+        then: "Parsed transaction is as expected"
+        // We can't do a byte-by-byte comparison because there is a random component to the signature
+
+        parsedTx.version == 1
+
+        parsedTx.inputs.size() == 1
+
+        with (parsedTx.inputs.get(0)) {
+            outpoint.hash == utxo_id
+            outpoint.index == 0
+            scriptBytes.length == 0x8a
+            scriptSig != null       // script needs to be broken down more and compared here
+            sequenceNumber == 0xffffffff
+        }
+
+        parsedTx.outputs.size() == 1
+
+        with (parsedTx.outputs.get(0)) {
+            value == txAmount
+            scriptBytes.length == 0x19
+            scriptPubKey != null    // script needs to be broken down more and compared here
+        }
+
+        parsedTx.lockTime == 0
     }
-
-
 }
