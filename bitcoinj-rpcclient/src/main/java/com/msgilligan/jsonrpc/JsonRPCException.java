6f5908753aaea59b1f84cd24165967308d0ce8bb
Sean Gilligan <sean@msgilligan.com>
2017-08-07 17:55:46 -0700

    Make JsonRPCException extend IOException

--- bitcoinj-rpcclient/src/main/java/com/msgilligan/jsonrpc/JsonRPCException.java
+++ bitcoinj-rpcclient/src/main/java/com/msgilligan/jsonrpc/JsonRPCException.java
@@ -1,9 +1,11 @@
 package com.msgilligan.jsonrpc;
 
+import java.io.IOException;
+
 /**
  * JSON RPC Exception
  */
-public class JsonRPCException extends Exception {
+public class JsonRPCException extends IOException {
 
     public JsonRPCException(String message) {
         super(message);
