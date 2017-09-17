2840b3ecb2b2c93fb439fad04e20c42de24a4a03
Sean Gilligan <sean@msgilligan.com>
2017-09-16 23:36:05 -0700

    Add PlantUML to AsciidocJavaDocs
    (Also update JavaDoc links a little)

--- bitcoinj-rpcclient/src/main/java/com/msgilligan/jsonrpc/package-info.java
+++ bitcoinj-rpcclient/src/main/java/com/msgilligan/jsonrpc/package-info.java
@@ -1,4 +1,29 @@
 /**
  * Basic JSON-RPC Java client and supporting classes.
+ *
+ * [plantuml, diagram-classes, png]
+ * ....
+ * skinparam packageStyle Rect
+ * skinparam shadowing false
+ * hide empty members
+ *
+ * namespace com.msgilligan.jsonrpc {
+ * interface UntypedRPCClient
+ * abstract class AbstractRPCClient
+ *
+ * class RPCClient
+ *
+ * interface DynamicRPCFallback << Groovy, trait >>
+ * class DynamicRPCClient << Groovy >>
+ *
+ * UntypedRPCClient <|.. AbstractRPCClient
+ * AbstractRPCClient <|-- RPCClient
+ *
+ * UntypedRPCClient <|.. DynamicRPCFallback
+ * RPCClient <|-- DynamicRPCClient
+ * DynamicRPCFallback <|.. DynamicRPCClient
+ * }
+ * ....
+ *
  */
 package com.msgilligan.jsonrpc;
