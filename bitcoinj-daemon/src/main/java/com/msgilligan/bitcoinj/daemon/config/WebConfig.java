94318755639f98d5e164e273ccc1b964bf3c523d
Sean Gilligan <sean@msgilligan.com>
2018-03-16 12:46:44 -0700

    Upgrade to Spring Boot 2.0

--- bitcoinj-daemon/src/main/java/com/msgilligan/bitcoinj/daemon/config/WebConfig.java
+++ bitcoinj-daemon/src/main/java/com/msgilligan/bitcoinj/daemon/config/WebConfig.java
@@ -1,11 +1,8 @@
 package com.msgilligan.bitcoinj.daemon.config;
 
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Spring Web configuration
