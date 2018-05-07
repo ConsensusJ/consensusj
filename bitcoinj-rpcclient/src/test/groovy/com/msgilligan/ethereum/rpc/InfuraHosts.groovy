23c131de75a5f30652f35bf4b09e2d404bc25361
Sean Gilligan <sean@msgilligan.com>
2018-05-06 19:17:55 -0700

    Fix InfuraHosts test support class

--- bitcoinj-rpcclient/src/test/groovy/com/msgilligan/ethereum/rpc/InfuraHosts.groovy
+++ bitcoinj-rpcclient/src/test/groovy/com/msgilligan/ethereum/rpc/InfuraHosts.groovy
@@ -1,7 +1,21 @@
 package com.msgilligan.ethereum.rpc
 
 /**
- *
+ * Container for Infura host URIs and API Key
  */
 class InfuraHosts {
+    public static String INFURA_API_KEY
+    public static URI INFURA_MAINNET_HOST
+    public static URI INFURA_ROPSTEN_HOST
+
+    static {
+        INFURA_API_KEY = "goes-here";
+        try {
+            INFURA_MAINNET_HOST = new URI("https://mainnet.infura.io/" + INFURA_API_KEY);
+            INFURA_ROPSTEN_HOST = new URI("https://ropsten.infura.io/" + INFURA_API_KEY);
+        } catch (URISyntaxException ignored) {
+            INFURA_MAINNET_HOST = null;
+            INFURA_ROPSTEN_HOST = null;
+        }
+    }
 }
