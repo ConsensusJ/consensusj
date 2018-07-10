0306199d4502f172e7ae586955825339d87de717
Sean Gilligan <sean@msgilligan.com>
2018-07-10 12:37:18 -0700

    Link to .puml file from JavaDoc/Asciidoclet
    (For preview while editing)

--- consensusj-jsonrpc/src/main/java/org/consensusj/jsonrpc/package-info.java
+++ consensusj-jsonrpc/src/main/java/org/consensusj/jsonrpc/package-info.java
@@ -3,34 +3,8 @@
  *
  * Note the Groovy classes are now in a separate module, but they're still in the diagram
  * to illustrate the purpose of `UntypedRPCClient`.
- * 
- * [plantuml, diagram-classes, png]
- * ....
- * skinparam packageStyle Rect
- * skinparam shadowing false
- * hide empty members
  *
- * namespace org.consensusj.jsonrpc {
- * interface UntypedRPCClient
- * abstract class AbstractRPCClient
- *
- * class RPCClient
- *
- * UntypedRPCClient <|.. AbstractRPCClient
- * AbstractRPCClient <|-- RPCClient
- *
- * }
- *
- * namespace org.consensusj.jsonrpc.groovy {
- *
- * interface DynamicRPCFallback << Groovy, trait >>
- * class DynamicRPCClient << Groovy >>
- *
- * org.consensusj.jsonrpc.UntypedRPCClient <|.. DynamicRPCFallback
- * org.consensusj.jsonrpc.RPCClient <|-- DynamicRPCClient
- * DynamicRPCFallback <|.. DynamicRPCClient
- * }
- * ....
+ * plantuml::doc/puml/JsonRpcClasses.puml[]
  *
  */
 package org.consensusj.jsonrpc;
