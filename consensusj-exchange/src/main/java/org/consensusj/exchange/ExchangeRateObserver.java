64bcd5e2850669b652270559acd74903df4e25a6
Sean Gilligan <sean@msgilligan.com>
2019-04-21 17:34:45 -0700

    Refactor exchange functions to consensusj-exchange
    * Give consensusj-currency and consensusj-exchange Automatic module names
    * Bump ConsensusJ version to 0.5.0-SNAPSHOT
    * Update README and CHANGELOG

--- consensusj-exchange/src/main/java/org/consensusj/exchange/ExchangeRateObserver.java
+++ consensusj-exchange/src/main/java/org/consensusj/exchange/ExchangeRateObserver.java
@@ -2,7 +2,7 @@ package org.consensusj.exchange;
 
 /**
  * Implement this interface to register with `ObservableExchangeRateProvider` for rate updates
- * TODO: Should we replace this with RxJava?
+ * TODO: Should we replace this with RxJava? Yes! Coming soon!
  */
 public interface ExchangeRateObserver {
     void onExchangeRateChange(ExchangeRateChange change);
