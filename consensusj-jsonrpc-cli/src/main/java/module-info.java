a46be66c47bb510a7bc4baf57468130370afe189
Sean Gilligan <sean@msgilligan.com>
2020-04-17 15:43:25 -0700

    Build o.c.jsonrpc.cli as Java Module
    Using Gradle 6.4 and the ‘extra-java-module-info’ plugin
    
    Remove moduleName variable not need by Gradle 6.4, remove Automatic-Module-Name
    property from the jar.

---
+++ consensusj-jsonrpc-cli/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * Module information
+ */
+module org.consensusj.jsonrpc.cli {
+    requires java.logging;
+    requires org.slf4j;
+
+    requires transitive org.apache.commons.cli;    // Non-modular, relies on processing by 'extra-java-module-info' plugin
+    requires transitive org.consensusj.jsonrpc;    // Automatic module from sibling subproject, not handled correctly by IntelliJ
+
+    exports org.consensusj.jsonrpc.cli;
+    exports org.consensusj.jsonrpc.cli.test;
+}
