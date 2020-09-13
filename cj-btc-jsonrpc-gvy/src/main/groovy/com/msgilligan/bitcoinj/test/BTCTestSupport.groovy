8e21e39101454fdbb0c6b88dc27a6142f68141d3
Sean Gilligan <sean@msgilligan.com>
2020-09-12 22:48:56 -0700

    Remove deprecated BTCTestSupport.btcToCoin method

--- cj-btc-jsonrpc-gvy/src/main/groovy/com/msgilligan/bitcoinj/test/BTCTestSupport.groovy
+++ cj-btc-jsonrpc-gvy/src/main/groovy/com/msgilligan/bitcoinj/test/BTCTestSupport.groovy
@@ -2,8 +2,6 @@ package com.msgilligan.bitcoinj.test
 
 import com.msgilligan.bitcoinj.rpc.BitcoinClientDelegate
 import org.consensusj.jsonrpc.groovy.Loggable
-import com.msgilligan.bitcoinj.json.conversion.BitcoinMath
-import org.bitcoinj.core.Coin
 
 /**
  * Test support functions intended to be mixed-in to Spock test specs
@@ -20,10 +18,5 @@ trait BTCTestSupport implements BitcoinClientDelegate, FundingSourceDelegate, Lo
 
     void consolidateCoins() {
         fundingSource.fundingSourceMaintenance();
-    }
-
-    @Deprecated
-    Coin btcToCoin(final BigDecimal btc) {
-        return BitcoinMath.btcToCoin(btc)
     }
 }
