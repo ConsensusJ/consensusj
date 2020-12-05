508bba6d2cfd3b9277dc65f6f80eeeb6b302bdef
Sean Gilligan <sean@msgilligan.com>
2020-12-04 21:54:42 -0800

    Fix BlockUtil.blockHeightFromCoinbase on SegWit

--- cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/BlockUtil.java
+++ cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/BlockUtil.java
@@ -7,6 +7,7 @@ import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptChunk;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 /**
  * Utility class containing {@link #blockHeightFromCoinbase(Block)} 
@@ -27,8 +28,19 @@ public class BlockUtil {
         }
         Transaction coinbase = block.getTransactions().get(0);
         byte[] scriptBytes = coinbase.getInput(0).getScriptBytes();
-        ScriptChunk chunk = new Script(scriptBytes).getChunks().get(0);
-        BigInteger height = new BigInteger(Utils.reverseBytes(chunk.data));
+        // There appears to be a bug in bitcoinj 0.15.8 for recent blocks where the input[0] script of the coinbase tx can't be parsed
+// The below code works for block 324140 but seems to fail on SegWit blocks
+//        Script script = new Script(scriptBytes);
+//        ScriptChunk chunk = script.getChunks().get(0);
+//        BigInteger height = new BigInteger(Utils.reverseBytes(chunk.data));
+        // We'll parse just the bytes we need as a work-around
+        BigInteger height;
+        byte[] blockNumBytes = Arrays.copyOfRange(scriptBytes, 1, 4);
+        if (scriptBytes[0] == 3) {
+            height = new BigInteger(Utils.reverseBytes(blockNumBytes));
+        } else {
+            height = BigInteger.valueOf(-1);
+        }
         return height.intValueExact();
     }
 }
