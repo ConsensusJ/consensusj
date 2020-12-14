f26ef2fddaa0ecbd6e9082dcf2ae700eac47574e
Sean Gilligan <sean@msgilligan.com>
2020-12-13 17:53:08 -0800

    cj-btc-rx: use Reactive Streams Publisher
    * Depend on Reactive Streams 1.0.3 rather than RxJava 3
    * Switch from RxJava 3 Flowable to Reactive Streams Publisher type

--- cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/RxBlockchainBinaryService.java
+++ cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/RxBlockchainBinaryService.java
@@ -1,6 +1,6 @@
 package org.consensusj.bitcoin.rx;
 
-import io.reactivex.rxjava3.core.Flowable;
+import org.reactivestreams.Publisher;
 
 import java.io.Closeable;
 
@@ -8,10 +8,13 @@ import java.io.Closeable;
  * For lower-level, higher-performance reactive subscriptions to blockchain data where we don't
  * want the overhead of parsing (and instantiating as a tree of objects) each block or transaction.
  * (e.g. a proxy or relay server)
+ *
+ *  Note: Implementation instances may throw {@link UnsupportedOperationException} if they don't support a particular
+ *  stream type.
  */
 public interface RxBlockchainBinaryService extends Closeable {
-    Flowable<byte[]> transactionBinaryPublisher();
-    Flowable<byte[]> transactionHashBinaryPublisher();
-    Flowable<byte[]> blockBinaryPublisher();
-    Flowable<byte[]> blockHashBinaryPublisher();
+    Publisher<byte[]> transactionBinaryPublisher();
+    Publisher<byte[]> transactionHashBinaryPublisher();
+    Publisher<byte[]> blockBinaryPublisher();
+    Publisher<byte[]> blockHashBinaryPublisher();
 }
--- cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/RxBlockchainService.java
+++ cj-btc-rx/src/main/java/org/consensusj/bitcoin/rx/RxBlockchainService.java
@@ -1,30 +1,29 @@
 package org.consensusj.bitcoin.rx;
 
-import io.reactivex.rxjava3.core.Flowable;
-import io.reactivex.rxjava3.core.Observable;
 import org.bitcoinj.core.Block;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
+import org.reactivestreams.Publisher;
 
 import java.io.Closeable;
 
 /**
- * RxJava 3 interface for subscribing to reactive blockchain data.
+ * Reactive Streams interface for subscribing to reactive blockchain data.
  *
  * There are at least 3 possible implementations
  * 1. A bitcoinj {@link org.bitcoinj.core.PeerGroup}
  * 2. The ZeroMQ service of a  trusted Bitcoin Core node
  * 3. A Bitcoin web service using WebSocket
  *
- * Note: Implementations may throw {@link UnsupportedOperationException} if they don't support a particular
- * observable.
+ * Note: Implementation instances may throw {@link UnsupportedOperationException} if they don't support a particular
+ * published data type.
  */
 public interface RxBlockchainService extends Closeable {
     NetworkParameters getNetworkParameters();
-    Flowable<Transaction> transactionPublisher();
-    Flowable<Sha256Hash> transactionHashPublisher();
-    Flowable<Block> blockPublisher();
-    Flowable<Sha256Hash> blockHashPublisher();
-    Flowable<Integer> blockHeightPublisher();
+    Publisher<Transaction> transactionPublisher();
+    Publisher<Sha256Hash> transactionHashPublisher();
+    Publisher<Block> blockPublisher();
+    Publisher<Sha256Hash> blockHashPublisher();
+    Publisher<Integer> blockHeightPublisher();
 }
