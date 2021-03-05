14cbddaf87859e2e6f5754e2e9ad1737819757a9
Sean Gilligan <sean@msgilligan.com>
2021-03-05 14:21:57 -0800

    Convert RpcURI from class to interface

--- cj-btc-jsonrpc/src/main/java/com/msgilligan/bitcoinj/rpc/RpcURI.java
+++ cj-btc-jsonrpc/src/main/java/com/msgilligan/bitcoinj/rpc/RpcURI.java
@@ -3,34 +3,35 @@ package com.msgilligan.bitcoinj.rpc;
 import java.net.URI;
 
 /**
- * Utility class with default connection information for Bitcoin JSON-RPC
+ * Interface with default connection information for Bitcoin JSON-RPC
  */
-public class RpcURI {
-    public static final String rpcproto = "http";
-    public static final String rpcssl = "https";
-    public static final String rpchost = "127.0.0.1";
-    public static final String rpcfile = "/";
+public interface RpcURI {
+    String rpcproto = "http";
+    String rpcssl = "https";
+    String rpchost = "127.0.0.1";
+    String rpcfile = "/";
 
-    public static final int RPCPORT_MAINNET = 8332;
-    public static final int RPCPORT_TESTNET = 18332;
-    public static final int RPCPORT_REGTEST = 18443;  // Was same port as TESTNET until Bitcoin Core 0.16.0
+    int RPCPORT_MAINNET = 8332;
+    int RPCPORT_TESTNET = 18332;
+    int RPCPORT_REGTEST = 18443;  // Was same port as TESTNET until Bitcoin Core 0.16.0
 
-    public static final URI DEFAULT_MAINNET_URI = createURI(rpcproto, rpchost, RPCPORT_MAINNET, rpcfile);
-    public static final URI DEFAULT_TESTNET_URI = createURI(rpcproto, rpchost, RPCPORT_TESTNET, rpcfile);
-    public static final URI DEFAULT_REGTEST_URI = createURI(rpcproto, rpchost, RPCPORT_REGTEST, rpcfile);
+    URI DEFAULT_MAINNET_URI = URI.create(rpcproto + "://" + rpchost + ":" + RPCPORT_MAINNET + rpcfile);
+    URI DEFAULT_TESTNET_URI = URI.create(rpcproto + "://" + rpchost + ":" + RPCPORT_TESTNET + rpcfile);
+    URI DEFAULT_REGTEST_URI = URI.create(rpcproto + "://" + rpchost + ":" + RPCPORT_REGTEST + rpcfile);
 
-    public static URI getDefaultMainNetURI() {
+    URI defaultMainNetURI = DEFAULT_MAINNET_URI;
+    URI defaultTestNetURI = DEFAULT_TESTNET_URI;
+    URI defaultRegTestURI = DEFAULT_REGTEST_URI;
+
+    static URI getDefaultMainNetURI() {
         return DEFAULT_MAINNET_URI;
     }
-    public static URI getDefaultTestNetURI() {
+
+    static URI getDefaultTestNetURI() {
         return DEFAULT_TESTNET_URI;
     }
-    public static URI getDefaultRegTestURI() {
+
+    static URI getDefaultRegTestURI() {
         return DEFAULT_REGTEST_URI;
     }
-
-    private static URI createURI(String proto, String host, int port, String file) {
-        return URI.create(rpcproto + "://" + rpchost + ":" + port + rpcfile);
-    }
-
 }
