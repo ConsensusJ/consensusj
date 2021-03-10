fff5d2d6d2a652417478342d523105a0131223de
Sean Gilligan <sean@msgilligan.com>
2021-03-10 15:08:52 -0800

    Basic test jsonrpc-echod with DynamicRpcClient

--- consensusj-jsonrpc-daemon/src/test/groovy/org/consensusj/jsonrpc/daemon/ApplicationSpec.groovy
+++ consensusj-jsonrpc-daemon/src/test/groovy/org/consensusj/jsonrpc/daemon/ApplicationSpec.groovy
@@ -1,7 +1,11 @@
 package org.consensusj.jsonrpc.daemon
 
 import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.consensusj.jsonrpc.JsonRpcMessage
+import org.consensusj.jsonrpc.JsonRpcResponse
+import org.consensusj.jsonrpc.groovy.DynamicRpcClient
 import spock.lang.Specification
 
 import jakarta.inject.Inject
@@ -12,11 +16,26 @@ import jakarta.inject.Inject
 @MicronautTest
 class ApplicationSpec extends Specification {
     @Inject
-    EmbeddedApplication<?> application
+    EmbeddedServer server
 
     void 'test it works'() {
         expect:
-        application.running
+        server.running
+        server.URI.getScheme() == "http"
+        server.URI.getHost() == "localhost"
+    }
+
+    void 'hit it with a JSON-RPC request'() {
+        given:
+        def testString = 'Hello jsonrpc-echod!'
+        def endpoint = URI.create(server.URI.toString()+"/jsonrpc")
+        def client = new DynamicRpcClient(JsonRpcMessage.Version.V2, endpoint, "", "")
+
+        when:
+        String result = client.echo(testString)
+
+        then:
+        result == testString
     }
 
 }
