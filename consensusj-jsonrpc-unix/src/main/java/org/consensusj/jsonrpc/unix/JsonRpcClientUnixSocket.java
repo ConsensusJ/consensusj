758b617ef00607462bbb531497d4d79edfb3961d
Sean Gilligan <sean@msgilligan.com>
2022-01-11 20:06:35 -0800

    jsonrpc-unix: client now works with c-lightningd
    * Update client to work with c-lightningd or echo server
    * Update echo server to return responses
    * Factor out “mapping” code to JsonRpcUnixSocketMapper

--- consensusj-jsonrpc-unix/src/main/java/org/consensusj/jsonrpc/unix/JsonRpcClientUnixSocket.java
+++ consensusj-jsonrpc-unix/src/main/java/org/consensusj/jsonrpc/unix/JsonRpcClientUnixSocket.java
@@ -1,12 +1,14 @@
 package org.consensusj.jsonrpc.unix;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.consensusj.jsonrpc.AbstractRpcClient;
 import org.consensusj.jsonrpc.JsonRpcMessage;
 import org.consensusj.jsonrpc.JsonRpcRequest;
 import org.consensusj.jsonrpc.JsonRpcResponse;
 import org.consensusj.jsonrpc.JsonRpcStatusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.StandardProtocolFamily;
@@ -15,25 +17,28 @@ import java.net.UnixDomainSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Optional;
 
 /**
- *
+ * Proof-of-concept UNIX domain socket JsonRpc Client (works with {@link UnixSocketEchoServer} and {@code lightningd}.)
  */
 public class JsonRpcClientUnixSocket extends AbstractRpcClient  {
+    private static final Logger log = LoggerFactory.getLogger(JsonRpcClientUnixSocket.class);
     private final UnixDomainSocketAddress socketAddress;
+    private final JsonRpcUnixSocketMapper socketMapper;
 
     public static void main(String[] args) throws IOException {
-        Path socketPath = UnixSocketEchoServer.getTestPath();
+        boolean useEchoServer = true;
+        Path socketPath = useEchoServer ? UnixSocketEchoServer.getTestPath() : getLightningRpcPath();
         JsonRpcClientUnixSocket socket  = new JsonRpcClientUnixSocket(socketPath);
-        JsonRpcRequest request = new JsonRpcRequest("echo", List.of("hello"));
-        String response = socket.send("echo", String.class, "hello" );
+        JsonNode response = socket.send("getinfo", JsonNode.class);
+        System.out.println(response);
     }
 
     public JsonRpcClientUnixSocket(Path socketPath) {
         super(JsonRpcMessage.Version.V2);
         socketAddress = UnixDomainSocketAddress.of(socketPath);
+        socketMapper = new JsonRpcUnixSocketMapper(mapper);
+
         // TODO: Delete on close:
         // Files.deleteIfExists(socketPath);
     }
@@ -43,47 +48,30 @@ public class JsonRpcClientUnixSocket extends AbstractRpcClient  {
         SocketChannel channel = SocketChannel.open(StandardProtocolFamily.UNIX);
         // TODO: Use Selectable channel for async??
         channel.connect(socketAddress);
-        ByteBuffer buffer = serialize(request);
+        ByteBuffer buffer = socketMapper.serializeRequest(request);
         while (buffer.hasRemaining()) {
             channel.write(buffer);
         }
         // TODO: Read response see https://www.baeldung.com/java-unix-domain-socket
         // See also: https://nipafx.dev/java-unix-domain-sockets/
         // And: https://www.linkedin.com/pulse/java-sockets-io-blocking-non-blocking-asynchronous-aliaksandr-liakh/
-        JsonRpcResponse<R> responseJson = readSocketResponse(request, responseType, channel);
+        JsonRpcResponse<R> responseJson = null;
+        try {
+            responseJson = socketMapper.readSocketResponse(request, responseType, channel);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         channel.close();
         return responseJson;
     }
 
-    private <R> JsonRpcResponse<R> readSocketResponse(JsonRpcRequest request, JavaType responseType, SocketChannel channel) {
-        JsonRpcResponse<R> responseJson = new JsonRpcResponse<R>(request, (R) "String result");
-        return responseJson;
-    }
-
-    private Optional<String> readSocketMessage(SocketChannel channel) throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        int bytesRead = channel.read(buffer);
-        if (bytesRead < 0)
-            return Optional.empty();
-
-        byte[] bytes = new byte[bytesRead];
-        buffer.flip();
-        buffer.get(bytes);
-        String message = new String(bytes);
-        return Optional.of(message);
-    }
-
-    private ByteBuffer serialize(JsonRpcRequest request) throws JsonProcessingException {
-        String message = mapper.writeValueAsString(request);
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        buffer.clear();
-        buffer.put(message.getBytes());
-        buffer.flip();
-        return buffer;
-    }
-
     @Override
     public URI getServerURI() {
-        return null;
+        return socketAddress.getPath().toUri();
+    }
+
+    public static Path getLightningRpcPath() {
+        return Path.of(System.getProperty("user.home")).resolve(".lightning/regtest/lightning-rpc");
+
     }
 }
--- consensusj-jsonrpc-unix/src/main/java/org/consensusj/jsonrpc/unix/UnixSocketEchoServer.java
+++ consensusj-jsonrpc-unix/src/main/java/org/consensusj/jsonrpc/unix/UnixSocketEchoServer.java
@@ -1,6 +1,11 @@
 package org.consensusj.jsonrpc.unix;
 
-import org.consensusj.jsonrpc.JsonRpcMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.consensusj.jsonrpc.JsonRpcError;
+import org.consensusj.jsonrpc.JsonRpcRequest;
+import org.consensusj.jsonrpc.JsonRpcResponse;
 
 import java.io.IOException;
 import java.net.StandardProtocolFamily;
@@ -10,13 +15,13 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 
 /**
  *
  */
 public class UnixSocketEchoServer {
     private final UnixDomainSocketAddress socketAddress;
+    private final JsonRpcUnixSocketMapper socketMapper;
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Path socketPath = getTestPath();
@@ -27,6 +32,8 @@ public class UnixSocketEchoServer {
 
     public UnixSocketEchoServer(Path socketPath) {
         socketAddress = UnixDomainSocketAddress.of(socketPath);
+        socketMapper = new JsonRpcUnixSocketMapper(getMapper());
+
         // TODO: Delete on close:
         // Files.deleteIfExists(socketPath);
     }
@@ -36,29 +43,44 @@ public class UnixSocketEchoServer {
         serverChannel.bind(socketAddress);
         SocketChannel channel = serverChannel.accept();
         while (true) {
-            readSocketMessage(channel)
-                    .ifPresent(message -> System.out.printf("[Client message] %s\n", message));
+            var optMessage = socketMapper.readSocketMessage(channel);
+            if (optMessage.isPresent()) {
+                processMessage(channel, optMessage.get());
+            }
             channel.close();
             channel = serverChannel.accept();
             Thread.sleep(100);
         }
     }
 
-    private Optional<String> readSocketMessage(SocketChannel channel) throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        int bytesRead = channel.read(buffer);
-        if (bytesRead < 0)
-            return Optional.empty();
+    private void processMessage(SocketChannel channel, String message) throws IOException {
+        System.out.printf("[Client message] %s\n", message);
+        JsonRpcRequest request;
+        try {
+            request = socketMapper.deserializeRequest(message);
+            System.out.println("Got " + request.getMethod() + " request");
+            JsonRpcResponse<?> response = switch (request.getMethod()) {
+                case "getinfo" -> new JsonRpcResponse<>(request, "Echo GETINFO Response");
+                default -> new JsonRpcResponse<>(request, JsonRpcError.of(JsonRpcError.Error.METHOD_NOT_FOUND));
+            };
+            ByteBuffer buffer = socketMapper.serializeResponse(response);
+            while (buffer.hasRemaining()) {
+                channel.write(buffer);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        byte[] bytes = new byte[bytesRead];
-        buffer.flip();
-        buffer.get(bytes);
-        String message = new String(bytes);
-        return Optional.of(message);
+    static ObjectMapper getMapper() {
+        var mapper = new ObjectMapper();
+        // TODO: Provide external API to configure FAIL_ON_UNKNOWN_PROPERTIES
+        // TODO: Remove "ignore unknown" annotations on various POJOs that we've defined.
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
     }
 
     static Path getTestPath() {
         return Path.of(System.getProperty("user.home")).resolve("consensusj.socket");
     }
-
 }
