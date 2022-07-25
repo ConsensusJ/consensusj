bb9fc5476c99122f7f92d9e42c98078de2a322ad
Sean Gilligan <sean@msgilligan.com>
2022-07-24 20:09:20 -0700

    Documentation: README, CHANGELOG, User Guide, JavDoc
    * CHANGELOG and README: Prepare for 0.6.0 release
    * User Guide: Improve documentation of JsonRpcClient class hierarchy
    * AbstractRpcClient: add TODO comment about how to undo our overuse of inheritance

--- cj-bitcoinj-util/src/main/java/org/consensusj/bitcoinj/wallet/package-info.java
+++ cj-bitcoinj-util/src/main/java/org/consensusj/bitcoinj/wallet/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Support for BIP-44 family HD Keychains, via {@link org.consensusj.bitcoinj.wallet.BipStandardDeterministicKeyChain}.
+ * Support for BIP-44 family HD Keychains, as a temporary solution until <b>bitcoinj</b> 0.17 is released.
  */
 package org.consensusj.bitcoinj.wallet;
