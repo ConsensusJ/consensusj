4d2a7fe97cdcbff1d3ddc81dfbca15768739d6f7
Sean Gilligan <sean@msgilligan.com>
2023-04-07 14:24:11 -0700

    Replace usage of Guava futures

--- cj-bitcoinj-util/src/main/java/org/consensusj/bitcoinj/future/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Adapter to classes for migrating from Guava futures to {@link java.util.concurrent.CompletableFuture}.
- * Hopefully these will be merged into the next release of <b>bitcoinj</b>.
- */
-package org.consensusj.bitcoinj.future;
