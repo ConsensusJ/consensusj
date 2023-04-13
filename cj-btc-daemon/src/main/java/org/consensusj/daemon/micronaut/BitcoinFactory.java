4ad159fa2b7d8f122d98d14773ac22654b55d924
Sean Gilligan <sean@msgilligan.com>
2023-04-13 09:33:44 -0700

    BitcoinFactory: set bitcoinj Context
    This is a workaround until bitcoinj removes the requirement this be set.

--- cj-btc-daemon/src/main/java/org/consensusj/daemon/micronaut/BitcoinFactory.java
+++ cj-btc-daemon/src/main/java/org/consensusj/daemon/micronaut/BitcoinFactory.java
@@ -3,6 +3,7 @@ package org.consensusj.daemon.micronaut;
 import com.fasterxml.jackson.databind.Module;
 import org.bitcoinj.base.BitcoinNetwork;
 import org.bitcoinj.base.ScriptType;
+import org.bitcoinj.core.Context;
 import org.bitcoinj.wallet.KeyChainGroupStructure;
 import org.consensusj.bitcoin.json.conversion.RpcServerModule;
 import io.micronaut.context.annotation.Factory;
@@ -28,6 +29,7 @@ public class BitcoinFactory {
 
     @Singleton
     public WalletAppKit getKit(BitcoinNetwork network) {
+        Context.propagate(new Context());
         // TODO: make File(".") and filePrefix configurable
         File directory = new File(".");
         String filePrefix = "BitcoinJDaemon";
