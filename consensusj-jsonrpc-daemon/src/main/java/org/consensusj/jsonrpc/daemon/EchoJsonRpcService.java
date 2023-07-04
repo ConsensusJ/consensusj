d44d0e15fd416cc4634f45e92c1b2747e4c78275
Sean Gilligan <sean@msgilligan.com>
2023-07-04 12:14:06 -0700

    EchoJsonRpcService.java: add `stop` command

--- consensusj-jsonrpc-daemon/src/main/java/org/consensusj/jsonrpc/daemon/EchoJsonRpcService.java
+++ consensusj-jsonrpc-daemon/src/main/java/org/consensusj/jsonrpc/daemon/EchoJsonRpcService.java
@@ -1,11 +1,17 @@
 package org.consensusj.jsonrpc.daemon;
 
+import io.micronaut.runtime.event.annotation.EventListener;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.runtime.server.event.ServerShutdownEvent;
+import io.micronaut.runtime.server.event.ServerStartupEvent;
 import org.consensusj.jsonrpc.introspection.AbstractJsonRpcService;
 import org.consensusj.jsonrpc.introspection.JsonRpcServiceWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.inject.Singleton;
+
+import java.io.Closeable;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -15,16 +21,36 @@ import java.util.concurrent.CompletableFuture;
  * Simple Echo JSON-RPC Service
  */
 @Singleton
-public class EchoJsonRpcService extends AbstractJsonRpcService {
+public class EchoJsonRpcService extends AbstractJsonRpcService implements Closeable {
     private static final Logger log = LoggerFactory.getLogger(EchoJsonRpcService.class);
     private static final Map<String, Method> methods = JsonRpcServiceWrapper.reflect(MethodHandles.lookup().lookupClass());
     private static final String helpString = """
             echo message
             help
+            stop
     """;
+
+    private EmbeddedServer embeddedServer;
 
     public EchoJsonRpcService() {
         super(methods);
+    }
+
+    @EventListener
+    public void onStartup(ServerStartupEvent event) {
+        log.info("Saving reference to embeddedServer");
+        embeddedServer = event.getSource();
+    }
+
+    @EventListener
+    public void onShutdown(ServerShutdownEvent event) {
+        log.info("Shutting down");
+        this.close();
+    }
+
+    @Override
+    public void close() {
+        log.info("Closing");
     }
 
     public CompletableFuture<String> echo(String message) {
@@ -35,5 +61,17 @@ public class EchoJsonRpcService extends AbstractJsonRpcService {
     public CompletableFuture<String> help() {
         log.debug("EchoJsonRpcService: help");
         return result(helpString);
+    }
+
+    /**
+     * Initiate server shutdown. This is a JSON-RPC method and will initiate but not
+     * complete server-shutdown because it must return a response to the client.
+     * @return A status string indicating the server is stopping
+     */
+    public CompletableFuture<String> stop() {
+        log.info("stop");
+        embeddedServer.stop();
+        var appName = embeddedServer.getApplicationConfiguration().getName().orElse("server");
+        return result(appName + " stopping");
     }
 }
