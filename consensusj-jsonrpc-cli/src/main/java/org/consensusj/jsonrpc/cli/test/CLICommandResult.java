0fa65aeacc54461bdaf0a81c90cb70061dbd8b42
Sean Gilligan <sean@msgilligan.com>
2025-12-27 12:11:43 -0800

    CLI test utils: use Charset, remove throws UnsupportedEncodingException

--- consensusj-jsonrpc-cli/src/main/java/org/consensusj/jsonrpc/cli/test/CLICommandResult.java
+++ consensusj-jsonrpc-cli/src/main/java/org/consensusj/jsonrpc/cli/test/CLICommandResult.java
@@ -1,16 +1,16 @@
 package org.consensusj.jsonrpc.cli.test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
  * Holds all the results (status code, stdout, stderr) of a CLI command run by a test
  */
 public record CLICommandResult(int status, String output, String error) {
-    static final String charset = StandardCharsets.UTF_8.toString();
+    static final Charset charset = StandardCharsets.UTF_8;
 
-    public CLICommandResult(int status, ByteArrayOutputStream os, ByteArrayOutputStream es) throws UnsupportedEncodingException {
+    public CLICommandResult(int status, ByteArrayOutputStream os, ByteArrayOutputStream es)  {
         this(status, os.toString(charset), es.toString(charset));
     }
 }
